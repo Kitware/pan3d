07469d849f28ad3c678426f72492dfcea2c8fbfd
Anne Haley <anne.haley@kitware.com>
2023-07-14 09:08:50 -0400

    Use single dockerfile for build (following example from trame/examples/deploy/docker/SingleFile)

--- src/docker/build.Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM kitware/trame
-
-RUN apt-get update && \
-    apt-get install --no-install-recommends --yes \
-    libosmesa6-dev \
-    && rm -rf /var/lib/apt/lists/*
--- src/docker/setup/initialize.sh
+++ src/docker/setup/initialize.sh
@@ -1,3 +1,3 @@
 # Install app from local directory
-pip install /opt/wheels/vtk-9.2.2.osmesa-*.whl
+pip install --extra-index-url https://wheels.vtk.org vtk-osmesa
 pip install /local-app
