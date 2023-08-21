76e878076c4036b679af670361aa2a17bdc7d9df
Anne Haley <anne.haley@kitware.com>
2023-08-21 13:42:13 -0400

    Fix test config

--- tests/test_config.py
+++ tests/test_config.py
@@ -3,5 +3,5 @@ from pan3d import DatasetBuilder  # noqa: F401
 
 def test_import_config():
     viewer = DatasetBuilder()
-    viewer.server.start()
     viewer.import_config("examples/example_config.json")
+    assert viewer.data_array.size == 184913280000
