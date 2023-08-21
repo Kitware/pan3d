9f2acb7ba64e9a7579a50d0cdb0cbbee9644beb1
Anne Haley <anne.haley@kitware.com>
2023-08-21 12:09:55 -0400

    Don't pass layout to components

--- pan3d/ui/render_area.py
+++ pan3d/ui/render_area.py
@@ -3,21 +3,18 @@ from pyvista.trame.ui import plotter_ui
 
 
 class RenderArea:
-    def __init__(self, layout):
-        ctrl = layout.server.controller
-        with layout:
-            with layout.content:
-                vuetify.VBanner(
-                    "{{ error_message }}",
-                    v_show=("error_message",),
-                )
-                with html.Div(
-                    v_show="array_active",
-                    style="height: 100%; position: relative;",
-                ):
-                    with plotter_ui(
-                        ctrl.get_plotter(),
-                        interactive_ratio=1,
-                    ) as plot_view:
-                        ctrl.view_update = plot_view.update
-                        ctrl.reset_camera = plot_view.reset_camera
+    def __init__(self, ctrl):
+        vuetify.VBanner(
+            "{{ error_message }}",
+            v_show=("error_message",),
+        )
+        with html.Div(
+            v_show="array_active",
+            style="height: 100%; position: relative; width: calc(100% - 300px)",
+        ):
+            with plotter_ui(
+                ctrl.get_plotter(),
+                interactive_ratio=1,
+            ) as plot_view:
+                ctrl.view_update = plot_view.update
+                ctrl.reset_camera = plot_view.reset_camera
--- pan3d/ui/toolbar.py
+++ pan3d/ui/toolbar.py
@@ -2,54 +2,48 @@ from trame.widgets import html, vuetify
 
 
 class Toolbar:
-    def __init__(self, layout):
-        ctrl = layout.server.controller
-        with layout:
-            with layout.toolbar:
-                layout.toolbar.dense = True
-                layout.toolbar.align = "center"
+    def __init__(self, ctrl):
+        vuetify.VProgressCircular(
+            v_show=("loading",),
+            indeterminate=True,
+            classes="mx-10",
+        )
 
-                vuetify.VProgressCircular(
-                    v_show=("loading",),
-                    indeterminate=True,
-                    classes="mx-10",
-                )
+        vuetify.VSpacer()
+        with vuetify.VBtn(
+            click=ctrl.reset,
+            v_show="unapplied_changes",
+            classes="mr-5",
+            small=True,
+        ):
+            html.Span("Apply & Render")
+            html.Span("({{ da_size }})", v_show="da_size")
 
-                vuetify.VSpacer()
-                with vuetify.VBtn(
-                    click=ctrl.reset,
-                    v_show="unapplied_changes",
-                    classes="mr-5",
-                    small=True,
-                ):
-                    html.Span("Apply & Render")
-                    html.Span("({{ da_size }})", v_show="da_size")
+        resolutions = [
+            0.001,
+            0.01,
+            0.1,
+            0.25,
+            0.5,
+            0.75,
+            1.0,
+        ]
+        vuetify.VSelect(
+            label="Resolution",
+            v_model=("resolution", 1.0),
+            v_show="array_active",
+            items=(resolutions,),
+            hide_details=True,
+            dense=True,
+            style="max-width: 100px",
+            classes="mt-3",
+        )
 
-                resolutions = [
-                    0.001,
-                    0.01,
-                    0.1,
-                    0.25,
-                    0.5,
-                    0.75,
-                    1.0,
-                ]
-                vuetify.VSelect(
-                    label="Resolution",
-                    v_model=("resolution", 1.0),
-                    v_show="array_active",
-                    items=(resolutions,),
-                    hide_details=True,
-                    dense=True,
-                    style="max-width: 100px",
-                    classes="mt-3",
-                )
-
-                vuetify.VCheckbox(
-                    v_model=("view_edge_visibility", True),
-                    v_show="array_active",
-                    dense=True,
-                    hide_details=True,
-                    on_icon="mdi-border-all",
-                    off_icon="mdi-border-outside",
-                )
+        vuetify.VCheckbox(
+            v_model=("view_edge_visibility", True),
+            v_show="array_active",
+            dense=True,
+            hide_details=True,
+            on_icon="mdi-border-all",
+            off_icon="mdi-border-outside",
+        )
