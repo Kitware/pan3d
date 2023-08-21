478af46b55a1d8e87be39ade88fadfcab4cee527
Anne Haley <anne.haley@kitware.com>
2023-08-21 14:18:17 -0400

    Remove reference to main class as `viewer`

--- pan3d/__main__.py
+++ pan3d/__main__.py
@@ -3,7 +3,7 @@ from dataset_builder import DatasetBuilder
 
 parser = ArgumentParser(
     prog="Pan3D",
-    description="Launch the Pan3D Dataset Viewer / Builder",
+    description="Launch the Pan3D Dataset Builder",
 )
 
 parser.add_argument("-b", "--config_path")
@@ -11,7 +11,7 @@ parser.add_argument("-d", "--dataset_path")
 
 args = parser.parse_args()
 
-viewer = DatasetBuilder(dataset_path=args.dataset_path)
+builder = DatasetBuilder(dataset_path=args.dataset_path)
 if args.config_path:
-    viewer.import_config(args.config_path)
-viewer.server.start()
+    builder.import_config(args.config_path)
+builder.server.start()
