0c36b45c413e22cc2a0963dde8eb4f875d848413
Anne Haley <anne.haley@kitware.com>
2023-08-21 14:42:16 -0400

    Dissolve RenderArea component into main class

--- pan3d/ui/__init__.py
+++ pan3d/ui/__init__.py
@@ -1,11 +1,9 @@
 from .axis_selection import AxisSelection
 from .main_drawer import MainDrawer
-from .render_area import RenderArea
 from .toolbar import Toolbar
 
 __all__ = [
     AxisSelection,
     MainDrawer,
-    RenderArea,
     Toolbar,
 ]
