fff5f80a1fae7758f84529d9efdb2953dacad571
Anne Haley <anne.haley@kitware.com>
2023-08-24 16:02:28 -0400

    Allow --server arg to main

--- pan3d/__main__.py
+++ pan3d/__main__.py
@@ -1,17 +1,18 @@
-from argparse import ArgumentParser
+import argparse
 from dataset_builder import DatasetBuilder
 
-parser = ArgumentParser(
+parser = argparse.ArgumentParser(
     prog="Pan3D",
     description="Launch the Pan3D Dataset Builder",
 )
 
 parser.add_argument("-b", "--config_path")
 parser.add_argument("-d", "--dataset_path")
+parser.add_argument("-s", "--server", action=argparse.BooleanOptionalAction)
 
 args = parser.parse_args()
 
 builder = DatasetBuilder(dataset_path=args.dataset_path)
 if args.config_path:
     builder.import_config(args.config_path)
-builder.viewer.server.start()
+builder.viewer.server.start(open_browser=not args.server)
