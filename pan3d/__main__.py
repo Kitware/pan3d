0fe727ed03b44935fc2812d175f3e368f0cfb767
Anne Haley <anne.haley@kitware.com>
2023-09-05 15:33:42 -0400

    Fix `pan3d-viewer` console script

--- pan3d/__main__.py
+++ pan3d/__main__.py
@@ -1,17 +1,19 @@
 from argparse import ArgumentParser
-from dataset_builder import DatasetBuilder
+from .dataset_builder import DatasetBuilder
 
-parser = ArgumentParser(
-    prog="Pan3D",
-    description="Launch the Pan3D Dataset Builder",
-)
 
-parser.add_argument("-b", "--config_path")
-parser.add_argument("-d", "--dataset_path")
+def main():
+    parser = ArgumentParser(
+        prog="Pan3D",
+        description="Launch the Pan3D Dataset Builder",
+    )
 
-args = parser.parse_args()
+    parser.add_argument("-b", "--config_path")
+    parser.add_argument("-d", "--dataset_path")
 
-builder = DatasetBuilder(dataset_path=args.dataset_path)
-if args.config_path:
-    builder.import_config(args.config_path)
-builder.viewer.server.start()
+    args = parser.parse_args()
+
+    builder = DatasetBuilder(dataset_path=args.dataset_path)
+    if args.config_path:
+        builder.import_config(args.config_path)
+    builder.viewer.server.start()
