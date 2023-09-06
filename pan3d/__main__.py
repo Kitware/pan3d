7286d610c5f604839cdc884fa0a1a3d446bf7885
Anne Haley <anne.haley@kitware.com>
2023-09-06 16:09:30 -0400

    Resolve merge conflicts

--- pan3d/__main__.py
+++ pan3d/__main__.py
@@ -1,17 +1,24 @@
-import argparse
-from dataset_builder import DatasetBuilder
+from argparse import ArgumentParser, BooleanOptionalAction
+from pan3d.dataset_builder import DatasetBuilder
 
-parser = argparse.ArgumentParser(
-    prog="Pan3D",
-    description="Launch the Pan3D Dataset Builder",
-)
 
-parser.add_argument("-b", "--config_path")
-parser.add_argument("-d", "--dataset_path")
-parser.add_argument("-s", "--server", action=argparse.BooleanOptionalAction)
-args = parser.parse_args()
+def main():
+    parser = ArgumentParser(
+        prog="Pan3D",
+        description="Launch the Pan3D Dataset Builder",
+    )
 
-builder = DatasetBuilder(dataset_path=args.dataset_path)
-if args.config_path:
-    builder.import_config(args.config_path)
-builder.viewer.server.start(open_browser=not args.server)
+    parser.add_argument("-b", "--config_path")
+    parser.add_argument("-d", "--dataset_path")
+    parser.add_argument("-s", "--server", action=BooleanOptionalAction)
+
+    args = parser.parse_args()
+
+    builder = DatasetBuilder(dataset_path=args.dataset_path)
+    if args.config_path:
+        builder.import_config(args.config_path)
+    builder.viewer.server.start()
+
+
+if __name__ == "__main__":
+    main()
