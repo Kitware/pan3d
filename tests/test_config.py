e7d0775384876c701c6822855e16db14fcdf2c2d
Anne Haley <anne.haley@kitware.com>
2023-09-20 14:15:31 -0400

    Add a new test to compare import & export

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,25 @@
+import json
+import tempfile
+from pathlib import Path
+
 from pan3d import DatasetBuilder  # noqa: F401
 
 
 def test_import_config():
     viewer = DatasetBuilder()
     viewer.import_config("examples/example_config.json")
-    assert viewer.data_array.size == 184913280000
+    assert viewer.data_array.size == 4622832
+
+
+def test_export_config():
+    viewer = DatasetBuilder()
+    import_path = "examples/example_config_2.json"
+    viewer.import_config(import_path)
+    with tempfile.TemporaryDirectory() as temp_dir:
+        export_path = Path(temp_dir, "exported.json")
+        viewer.export_config(export_path)
+        with open(import_path) as f:
+            imported = json.load(f)
+        with open(export_path) as f:
+            exported = json.load(f)
+        assert imported == exported
