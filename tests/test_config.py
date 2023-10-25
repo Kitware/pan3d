65ceb6f0c3a4cc3a4b6daf33dceab9923194fb50
Anne Haley <anne.haley@kitware.com>
2023-10-25 10:06:38 -0400

    Change expected size for sliced array in test_import_config

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,7 +8,8 @@ from pan3d import DatasetBuilder  # noqa: F401
 def test_import_config():
     viewer = DatasetBuilder()
     viewer.import_config("examples/example_config_noaa.json")
-    assert viewer.data_array.size == 4622832
+    # With slicing, data_array has shape (18, 36)
+    assert viewer.data_array.size == 648
 
 
 def test_export_config():
