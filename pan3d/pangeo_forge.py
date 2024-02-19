3ed0b29ece034dc1ad7788a3e12a3eac231c682f
Anne Haley <anne.haley@kitware.com>
2024-02-19 17:29:45 +0000

    refactor(pangeo): use methods similar to esgf module

--- pan3d/pangeo_forge.py
+++ pan3d/pangeo_forge.py
@@ -1,4 +1,18 @@
+import json
+import xarray
 import requests
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).parent
+CACHED_CATALOG_PATH = Path(BASE_DIR, "../examples/pangeo_catalog.json")
+GROUPS = [
+    {'name': 'Pangeo - CMIP6', 'value': 'cmip6',},
+    {'name': 'Pangeo - CMIP6 Static', 'value': 'cmip6_static',},
+    {'name': 'Pangeo - CMIP6 PMIP', 'value': 'cmip6_pmip',},
+    {'name': 'Pangeo - EOBS', 'value': 'eobs',},
+    {'name': 'Pangeo - Miscellaneous', 'value': 'misc',},
+]
 
 
 def get_catalog():
@@ -25,9 +39,19 @@ def get_catalog():
                             {
                                 "name": item_name,
                                 "id": item["id"],
-                                "url": item["dataset_public_url"],
+                                "value": item["dataset_public_url"],
                                 "more_info": f'https://pangeo-forge.org/dashboard/feedstock/{feedstock["id"]}',
                             }
                         )
     print(f"Retrieved {len(catalog)} viable datasets.")
     return catalog
+
+
+def get_group_datasets(group_id):
+    # Use cached catalog
+    with open(CACHED_CATALOG_PATH) as f:
+        catalog_contents = json.load(f)
+        return catalog_contents.get(group_id, [])
+
+def load_dataset(dataset_id):
+    return xarray.open_dataset(dataset_id, engine='zarr')
