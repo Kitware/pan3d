1b1f0ba2f51f01481623a574d78f1981c8d685fe
Anne Haley <anne.haley@kitware.com>
2024-02-19 17:34:22 +0000

    feat: add `--esgf` argument to `pan3d-viewer`

--- pan3d/serve_viewer.py
+++ pan3d/serve_viewer.py
@@ -11,12 +11,17 @@ def serve():
     parser.add_argument("-C", "--config_path")
     parser.add_argument("-D", "--dataset")
     parser.add_argument("-P", "--pangeo", action=BooleanOptionalAction)
+    parser.add_argument("-E", "--esgf", action=BooleanOptionalAction)
     parser.add_argument("-S", "--server", action=BooleanOptionalAction)
     parser.add_argument("-d", "--debug", action=BooleanOptionalAction)
 
     args = parser.parse_args()
 
-    builder = DatasetBuilder(dataset_path=args.dataset, pangeo=args.pangeo)
+    builder = DatasetBuilder(
+        dataset=args.dataset,
+        pangeo=args.pangeo,
+        esgf=args.esgf,
+    )
     if args.config_path:
         builder.import_config(args.config_path)
     builder.viewer.start(debug=args.debug)
