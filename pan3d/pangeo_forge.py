84a2406830d37ff156f01b83e392de311b4b5d1d
Anne Haley <anne.haley@kitware.com>
2024-02-23 15:26:11 +0000

    wip: update Pangeo module with congruent API to ESGF module; functions still need implementation

--- pan3d/pangeo_forge.py
+++ pan3d/pangeo_forge.py
@@ -1,35 +1,31 @@
-import json
 import xarray
-import requests
-from pathlib import Path
-
-
-BASE_DIR = Path(__file__).parent
-CACHED_CATALOG_PATH = Path(BASE_DIR, "../examples/pangeo_catalog.json")
-GROUPS = [
-    {'name': 'Pangeo - CMIP6', 'value': 'cmip6',},
-    {'name': 'Pangeo - CMIP6 Static', 'value': 'cmip6_static',},
-    {'name': 'Pangeo - CMIP6 PMIP', 'value': 'cmip6_pmip',},
-    {'name': 'Pangeo - EOBS', 'value': 'eobs',},
-    {'name': 'Pangeo - Miscellaneous', 'value': 'misc',},
-]
+from datetime import datetime
 
 
 def get_catalog():
     return {
-        'name': 'Pangeo Forge',
-        'id': 'pangeo',
-        'search_terms': [
-            {'key': 'id', 'options': []}
-        ]
+        "name": "Pangeo Forge",
+        "id": "pangeo",
+        "search_terms": [{"key": "id", "options": []}],
     }
 
 
-def get_group_datasets(group_id):
-    # Use cached catalog
-    with open(CACHED_CATALOG_PATH) as f:
-        catalog_contents = json.load(f)
-        return catalog_contents.get(group_id, [])
+def get_catalog_search_options():
+    search_options = {}
+    return search_options
+
+
+def search_catalog(**kwargs):
+    group_name = "/".join(list(",".join(v) for v in kwargs.values()))
+    start = datetime.now()
+    results = []
+
+    delta = datetime.now() - start
+    message = f'Found {len(results)} dataset ids \
+        in {delta.total_seconds()} seconds.\
+        Results added to group "{group_name}".'
+    return (results, group_name, message)
+
 
 def load_dataset(dataset_id):
-    return xarray.open_dataset(dataset_id, engine='zarr')
+    return xarray.open_dataset(dataset_id, engine="zarr")
