4b82c451c130c9eaf857f37fc37644716e2f8e19
Anne Haley <anne.haley@kitware.com>
2024-02-23 14:45:29 +0000

    wip: update Pangeo module; needs catalog search functions

--- pan3d/pangeo_forge.py
+++ pan3d/pangeo_forge.py
@@ -16,35 +16,13 @@ GROUPS = [
 
 
 def get_catalog():
-    print("fetching Pangeo Forge catalog...")
-    catalog = []
-    feedstocks = requests.get("https://api.pangeo-forge.org/feedstocks/").json()
-    for feedstock in feedstocks:
-        if feedstock["provider"] == "github":
-            data = requests.get(
-                f"https://api.pangeo-forge.org/feedstocks/{feedstock['id']}/datasets"
-            ).json()
-            for item in data:
-                if (
-                    not item["is_test"]
-                    and item["dataset_public_url"]
-                    and "https://" in item["dataset_public_url"]
-                    and item["dataset_type"] == "zarr"
-                ):
-                    item_name = feedstock["spec"].split("/")[-1]
-                    if len(data) > 1:
-                        item_name += f' - {item["recipe_id"]}'
-                    if item_name not in [i["name"] for i in catalog]:
-                        catalog.append(
-                            {
-                                "name": item_name,
-                                "id": item["id"],
-                                "value": item["dataset_public_url"],
-                                "more_info": f'https://pangeo-forge.org/dashboard/feedstock/{feedstock["id"]}',
-                            }
-                        )
-    print(f"Retrieved {len(catalog)} viable datasets.")
-    return catalog
+    return {
+        'name': 'Pangeo Forge',
+        'id': 'pangeo',
+        'search_terms': [
+            {'key': 'id', 'options': []}
+        ]
+    }
 
 
 def get_group_datasets(group_id):
