b098879095feaf3675823ab7cfff1abe13499182
Anne Haley <anne.haley@kitware.com>
2024-07-16 13:01:23 +0000

    fix: Close menus for bounds configure and render options when nearby drawer changes state

--- pan3d/utils.py
+++ pan3d/utils.py
@@ -74,6 +74,8 @@ initial_state = {
     "ui_import_loading": False,
     "ui_main_drawer": False,
     "ui_axis_drawer": False,
+    "ui_bounds_menu": False,
+    "ui_render_options_menu": False,
     "ui_unapplied_changes": False,
     "ui_error_message": None,
     "ui_more_info_link": None,
