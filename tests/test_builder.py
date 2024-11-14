e8f3b72a8ae140272c37cd4666e820ba29a14776
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2024-11-14 09:14:06 -0700

    ci: update testing and ci

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -1,208 +1,50 @@
 import json
-import tempfile
 from pathlib import Path
-import pytest
 
-from pan3d import DatasetBuilder
+from pan3d.xarray.algorithm import vtkXArrayRectilinearSource
+
+ROOT_PATH = Path(__file__).parent.parent.resolve()
+
+
+def read_config(path):
+    return json.loads((ROOT_PATH / path).read_text())
 
 
 def test_import_config():
-    builder = DatasetBuilder()
-    builder.import_config("examples/example_config_noaa.json")
-    # With slicing, data_array has shape (100, 100)
-    assert builder.data_array.size == 10000
+    conf = read_config("examples/example_config_noaa.json")
+    builder = vtkXArrayRectilinearSource()
+    builder.load(conf)
+
+    # check available arrays
+    assert set(builder.available_arrays) == set(
+        ["sea_ice_fraction", "analysed_sst", "mask", "analysis_error"]
+    )
+
+    # check array size based on slicing
+    builder.arrays = ["mask"]
+    ds = builder()
+    assert ds.point_data["mask"].size == 10000
 
 
 def test_export_config():
-    builder = DatasetBuilder()
-    import_path = "examples/example_config_xarray.json"
-    builder.import_config(import_path)
-    with tempfile.TemporaryDirectory() as temp_dir:
-        export_path = Path(temp_dir, "exported.json")
-        builder.export_config(export_path)
-        with open(import_path) as f:
-            imported = json.load(f)
-        with open(export_path) as f:
-            exported = json.load(f)
-
-        # Disregard ui section
-        imported.pop("ui", None)
-        exported.pop("ui", None)
-        assert imported == exported
+    conf = read_config("examples/example_config_xarray.json")
+    builder = vtkXArrayRectilinearSource()
+    builder.load(conf)
+    input_conf = conf.get("dataset_config")
+    output_conf = builder.state.get("dataset_config")
+    input_conf["arrays"] = set(input_conf["arrays"])
+    output_conf["arrays"] = set(output_conf["arrays"])
+    assert input_conf == output_conf
 
 
 def test_setters():
-    builder = DatasetBuilder()
+    builder = vtkXArrayRectilinearSource()
 
-    builder.dataset_info = {"source": "xarray", "id": "eraint_uvz"}
-    # builder will auto select the following:
-    # builder.data_array_name = 'z'
-    # builder.x = 'longitude'
-    # builder.y = 'latitude'
-    builder.z = "level"
-    builder.t = "month"
+    builder.load({"data_origin": {"source": "xarray", "id": "eraint_uvz"}})
     builder.t_index = 1
-    builder.slicing = {"longitude": [0, 90, 2]}
 
-    assert builder.dataset_info == {"source": "xarray", "id": "eraint_uvz"}
-    assert builder.dataset is not None
-    assert builder.data_array_name == "z"
     assert builder.x == "longitude"
     assert builder.y == "latitude"
     assert builder.z == "level"
     assert builder.t == "month"
     assert builder.t_index == 1
-
-
-def test_setters_invalid_values():
-    builder = DatasetBuilder()
-
-    # Setting wrong types
-    with pytest.raises(TypeError) as e:
-        builder.dataset_info = "foo"
-    assert str(e.value) == "Type of dataset_info must be Dict or None."
-    with pytest.raises(TypeError) as e:
-        builder.dataset = "foo"
-    assert str(e.value) == "Type of dataset must be xarray.Dataset or None."
-    with pytest.raises(TypeError) as e:
-        builder.data_array_name = 2
-    assert str(e.value) == "Type of data_array_name must be str or None."
-    with pytest.raises(TypeError) as e:
-        builder.x = 2
-    assert str(e.value) == "Type of x must be str or None."
-    with pytest.raises(TypeError) as e:
-        builder.y = 2
-    assert str(e.value) == "Type of y must be str or None."
-    with pytest.raises(TypeError) as e:
-        builder.z = 2
-    assert str(e.value) == "Type of z must be str or None."
-    with pytest.raises(TypeError) as e:
-        builder.t = 2
-    assert str(e.value) == "Type of t must be str or None."
-    with pytest.raises(TypeError) as e:
-        builder.t_index = None
-    assert str(e.value) == "Type of t_index must be int."
-    with pytest.raises(TypeError) as e:
-        builder.slicing = "foo"
-    assert str(e.value) == "Type of slicing must be Dict or None."
-
-    # Setting in the wrong order
-    with pytest.raises(ValueError) as e:
-        builder.t_index = 5
-    assert str(e.value) == "Cannot set time index > 0 without setting t array first."
-    with pytest.raises(ValueError) as e:
-        builder.t = "foo"
-    assert str(e.value) == "Cannot set t without setting data array name first."
-    with pytest.raises(ValueError) as e:
-        builder.z = "foo"
-    assert str(e.value) == "Cannot set z without setting data array name first."
-    with pytest.raises(ValueError) as e:
-        builder.y = "foo"
-    assert str(e.value) == "Cannot set y without setting data array name first."
-    with pytest.raises(ValueError) as e:
-        builder.x = "foo"
-    assert str(e.value) == "Cannot set x without setting data array name first."
-    with pytest.raises(ValueError) as e:
-        builder.slicing = {"foo": [0, 1, 1]}
-    assert str(e.value) == "Cannot set slicing without setting data array name first."
-    with pytest.raises(ValueError) as e:
-        builder.data_array_name = "foo"
-    assert (
-        str(e.value) == "Cannot set data array name without setting dataset info first."
-    )
-
-    # Setting wrong values for dataset_info
-    with pytest.raises(ValueError) as e:
-        builder.dataset_info = {}
-    assert str(e.value) == 'Dataset info must contain key "id" with string value.'
-    with pytest.raises(ValueError) as e:
-        builder.dataset_info = {"id": "foo", "source": "bar"}
-    assert (
-        str(e.value)
-        == "Invalid source value. Must be one of [default, xarray, pangeo, esgf]."
-    )
-
-    # Set a valid value to proceed
-    builder.dataset_info = {"source": "xarray", "id": "eraint_uvz"}
-
-    # Setting wrong values for data_array_name
-    with pytest.raises(ValueError) as e:
-        builder.data_array_name = "foo"
-    assert (
-        str(e.value) == "foo does not exist on dataset. Must be one of ['z', 'u', 'v']."
-    )
-
-    # Set a valid value to proceed
-    builder.data_array_name = "v"
-
-    acceptable_coord_names = ["latitude", "level", "longitude", "month"]
-    # Setting wrong values for x, y, z, t
-    with pytest.raises(ValueError) as e:
-        builder.x = "foo"
-    assert (
-        str(e.value)
-        == f"foo does not exist on data array. Must be one of {acceptable_coord_names}."
-    )
-    with pytest.raises(ValueError) as e:
-        builder.y = "foo"
-    assert (
-        str(e.value)
-        == f"foo does not exist on data array. Must be one of {acceptable_coord_names}."
-    )
-    with pytest.raises(ValueError) as e:
-        builder.z = "foo"
-    assert (
-        str(e.value)
-        == f"foo does not exist on data array. Must be one of {acceptable_coord_names}."
-    )
-    with pytest.raises(ValueError) as e:
-        builder.t = "foo"
-    assert (
-        str(e.value)
-        == f"foo does not exist on data array. Must be one of {acceptable_coord_names}."
-    )
-
-    # Set valid values to proceed
-    builder.x = "longitude"
-    builder.y = "latitude"
-    builder.z = "level"
-    builder.t = "month"
-
-    # Setting wrong values for t_index
-    with pytest.raises(ValueError) as e:
-        builder.t_index = -1
-    assert str(e.value) == "Time index must be a positive integer."
-    with pytest.raises(ValueError) as e:
-        builder.t_index = 100
-    assert str(e.value) == "Time index must be less than size of t coordinate (2)."
-
-    # Setting wrong values for slicing
-    with pytest.raises(ValueError) as e:
-        builder.slicing = {0: []}
-    assert str(e.value) == "Keys in slicing must be strings."
-    with pytest.raises(ValueError) as e:
-        builder.slicing = {"foo": []}
-    assert (
-        str(e.value)
-        == "Values in slicing must be lists of 3 integers ([start, stop, step])."
-    )
-    with pytest.raises(ValueError) as e:
-        builder.slicing = {"foo": [0, 1, 1]}
-    assert (
-        str(e.value)
-        == f"Key foo not found in data array. Must be one of {acceptable_coord_names}."
-    )
-    with pytest.raises(ValueError) as e:
-        builder.slicing = {"month": [-1, 10, 10]}
-    assert (
-        str(e.value)
-        == "Value [-1, 10, 10] not applicable for Key month. Step value must be <= 2."
-    )
-
-
-# def test_import_error():
-#     # This will only succeed in an environment where
-#     # pan3d is installed but pan3d[geotrame] is not installed.
-#     builder = DatasetBuilder()
-#     with pytest.raises(ImportError):
-#         builder.viewer
--- tests/test_viewer.py
+++ tests/test_viewer.py
@@ -1,40 +1,13 @@
-from pan3d import DatasetBuilder
-from pan3d import DatasetViewer
-
-
-def push_builder_state(builder):
-    builder.dataset_info = {"source": "xarray", "id": "eraint_uvz"}
-    builder.z = "level"
-    builder.t = "month"
-    builder.t_index = 1
-    builder.slicing = {"longitude": [0, 90, 1], "latitude": [0, 100, 1]}
-
-
-def push_viewer_state(viewer):
-    # For state updates with callbacks,
-    # Ensure the callbacks occur in the correct order
-    viewer.state.update(dict(dataset_info={"source": "xarray", "id": "eraint_uvz"}))
-    viewer.state.flush()
-    viewer.state.update(
-        dict(
-            da_active="z",
-        )
-    )
-    viewer.state.flush()
-    viewer.state.update(
-        dict(
-            da_z="level",
-            da_t="month",
-            da_t_index=1,
-        )
-    )
-    viewer.state.flush()
+from pan3d.viewers.preview import XArrayViewer
 
 
 def assert_builder_state(builder):
-    assert builder.dataset_info == {"source": "xarray", "id": "eraint_uvz"}
-    assert builder.dataset is not None
-    assert builder.data_array_name == "z"
+    assert builder.state.get("data_origin") == {
+        "source": "xarray",
+        "id": "eraint_uvz",
+        "order": "C",
+    }
+    assert builder.arrays == ["z"]
     assert builder.x == "longitude"
     assert builder.y == "latitude"
     assert builder.z == "level"
@@ -42,143 +15,61 @@ def assert_builder_state(builder):
     assert builder.t_index == 1
 
 
-def assert_viewer_state(viewer):
-    assert viewer.state.dataset_info == {"source": "xarray", "id": "eraint_uvz"}
-    assert viewer.state.dataset_ready
-    assert viewer.state.da_active == "z"
-    assert viewer.state.da_x == "longitude"
-    assert viewer.state.da_y == "latitude"
-    assert viewer.state.da_z == "level"
-    assert viewer.state.da_t == "month"
-    assert viewer.state.da_t_index == 1
-    assert viewer.state.da_size == "211 KB"
-    assert viewer.state.da_vars == [
-        {"name": "z", "id": 0},
-        {"name": "u", "id": 1},
-        {"name": "v", "id": 2},
-    ]
-
-
 def test_ui_state():
-    viewer = DatasetViewer(server="ui", state=dict(render_auto=False))
-    push_viewer_state(viewer)
+    viewer = XArrayViewer(server="test")
 
-    viewer._coordinate_toggle_expansion("month")
-    viewer._coordinate_toggle_expansion("level")
+    # fake server started to trigger callback
+    viewer.state.ready()
 
-    assert not viewer.state.ui_loading
-    assert not viewer.state.ui_main_drawer
-    assert not viewer.state.ui_axis_drawer
-    assert viewer.state.ui_unapplied_changes
-    assert viewer.state.ui_error_message is None
-    assert viewer.state.ui_more_info_link is None
-    assert viewer.state.ui_expanded_coordinates == ["month", "level"]
+    viewer.import_state(
+        {
+            "data_origin": {"source": "xarray", "id": "eraint_uvz"},
+            "dataset_config": {
+                "arrays": ["z"],
+                "t_index": 1,
+                "slices": {
+                    "longitude": [0, 90, 1],
+                    "latitude": [0, 100, 1],
+                },
+            },
+        }
+    )
+    assert_builder_state(viewer.source)
+    assert viewer.state.axis_names == ["longitude", "latitude", "level"]
+    assert viewer.state.data_origin_error is False
+    assert viewer.state.data_origin_source == "xarray"
+    assert viewer.state.data_origin_id == "eraint_uvz"
+    assert viewer.state.load_button_text == "Loaded"
+    assert viewer.state.can_load is False
+    assert viewer.state.show_data_information is True
+    assert len(viewer.state.t_labels) == 2
 
 
 def test_render_options_state():
-    viewer = DatasetViewer(server="render_options", state=dict(render_auto=False))
-    push_viewer_state(viewer)
+    viewer = XArrayViewer(server="render_options")
 
-    viewer.set_render_scales(
-        x=2,
-        y=3,
-        z=4,
+    # fake server started to trigger callback
+    viewer.state.ready()
+
+    viewer.import_state(
+        {
+            "data_origin": {"source": "xarray", "id": "eraint_uvz"},
+            "dataset_config": {
+                "arrays": ["z"],
+                "t_index": 1,
+            },
+        }
     )
-    viewer.set_render_options(
-        colormap="magma",
-        transparency=True,
-        transparency_function="linear_r",
-        scalar_warp=True,
-        cartographic=False,  # not compatible with this 4D data
-        # geovista GeoPlotter includes a check for GPU availability,
-        # which fails on GH Actions. Disable render in this function.
-        render=False,
-    )
+    with viewer.state:
+        viewer.state.update(
+            {
+                "scale_x": 0.1,
+                "scale_y": 0.2,
+                "scale_z": 0.3,
+                "color_by": "z",
+            }
+        )
 
-    assert viewer.state.render_x_scale == 2
-    assert viewer.state.render_y_scale == 3
-    assert viewer.state.render_z_scale == 4
-    assert viewer.state.render_colormap == "magma"
-    assert viewer.state.render_transparency
-    assert viewer.state.render_transparency_function == "linear_r"
-    assert viewer.state.render_scalar_warp
-    assert not viewer.state.render_cartographic
-
-
-def test_viewer_export():
-    viewer = DatasetViewer(server="export", state=dict(render_auto=False))
-    push_viewer_state(viewer)
-
-    viewer.state.update(dict(ui_action_name="Export"))
-    viewer.state.flush()
-
-    # Export action will complete on flush and reset action state
-    assert viewer.state.ui_action_name == "Export"
-    assert viewer.state.ui_action_message is None
-    assert viewer.state.ui_action_config_file is None
-
-    assert viewer.state.state_export["data_origin"] == {
-        "source": "xarray",
-        "id": "eraint_uvz",
-    }
-    assert viewer.state.state_export["data_array"]["name"] == "z"
-    assert viewer.state.state_export["data_array"]["x"] == "longitude"
-    assert viewer.state.state_export["data_array"]["y"] == "latitude"
-    assert viewer.state.state_export["data_array"]["z"] == "level"
-    assert viewer.state.state_export["data_array"]["t"] == "month"
-    assert viewer.state.state_export["data_array"]["t_index"] == 1
-    assert viewer.state.state_export["data_slices"]["longitude"] == [0, 480, 4]
-    assert viewer.state.state_export["data_slices"]["latitude"] == [0, 241, 2]
-    assert viewer.state.state_export["data_slices"]["level"] == [0, 3, 1]
-    assert viewer.state.state_export["data_slices"]["month"] == [0, 2, 1]
-    assert not viewer.state.state_export["ui"]["main_drawer"]
-    assert not viewer.state.state_export["ui"]["axis_drawer"]
-    assert viewer.state.state_export["ui"]["unapplied_changes"]
-    assert viewer.state.state_export["ui"]["error_message"] is None
-    assert viewer.state.state_export["ui"]["more_info_link"] is None
-    assert viewer.state.state_export["ui"]["expanded_coordinates"] == []
-
-
-def test_layout():
-    from trame_vuetify.ui.vuetify3 import VAppLayout
-
-    builder = DatasetBuilder(server="layout")
-    assert isinstance(builder.viewer.ui, VAppLayout)
-
-
-def test_sync_to_viewer_from_builder():
-    builder = DatasetBuilder(server="from_builder")
-    viewer = builder.viewer
-    viewer.state.render_auto = False
-    push_builder_state(builder)
-
-    assert_builder_state(builder)
-    assert_viewer_state(viewer)
-
-
-def test_sync_during_viewer_creation():
-    builder = DatasetBuilder(server="from_creation")
-    push_builder_state(builder)
-    assert_builder_state(builder)
-
-    # Viewer created last, state should sync during initialization
-    viewer = builder.viewer
-    viewer.state.render_auto = False
-    assert_viewer_state(viewer)
-
-
-def test_sync_from_viewer_ui_functions():
-    builder = DatasetBuilder(server="from_ui_funcs")
-    viewer = builder.viewer
-    viewer.state.render_auto = False
-    push_viewer_state(viewer)
-
-    viewer._coordinate_select_axis("level", None, "da_z")
-    viewer._coordinate_select_axis("month", None, "da_t")
-    viewer._coordinate_change_slice("longitude", "start", 0)
-    viewer._coordinate_change_slice("longitude", "stop", 89)
-    viewer._coordinate_change_bounds("latitude", [0, 99])
-
-    viewer.state.flush()
-    assert_builder_state(builder)
-    assert_viewer_state(viewer)
+    assert_builder_state(viewer.source)
+    assert viewer.actor.GetScale() == (0.1, 0.2, 0.3)
+    assert viewer.mapper.array_name == "z"
